d5bf2ddf5c93d8a81a2c471fa33646a523ade2a5
bhusalashish <ashish.bhusal.16999@gmail.com>
2021-01-23 01:12:40 +0530

    --bug_fixes Login with Github

--- client/src/components/LoginwithGithub.js
+++ client/src/components/LoginwithGithub.js
@@ -1,29 +1,30 @@
 import React from "react";
 import GitHubLogin from "react-github-login";
-import axios from 'axios'
+import axios from "axios";
 
 function LoginwithGithub() {
     const onSuccess = (response) => {
+        console.log(response);
         axios({
-            method:"POST",
-            url:"http://localhost:4000/api/user/logingithub",
-            data:{code:response.code}
-          }).then(response=>{
-      
-            console.log(response.data.message.msgBody)
-      
-          })
-          .catch(err=>console.log(err))
-    }
+            method: "POST",
+            url: "http://localhost:4000/api/user/login/github",
+            data: { code: response.code },
+        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => console.log(err));
+    };
     const onFailure = (response) => console.error(response);
     return (
         <GitHubLogin
             clientId="69ab3843cec73ec55290"
             onSuccess={onSuccess}
             onFailure={onFailure}
-            redirectUri="http://localhost:3000/home"
+            scope={"read:user,user:email"}
+            redirectUri="http://localhost:3000"
         />
-    ); 
+    );
 }
 
 export default LoginwithGithub;
